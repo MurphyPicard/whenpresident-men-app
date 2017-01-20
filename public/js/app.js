43b5f3d09b5225e8324d5484fc403616006ba5e9
murphypicard <ayaghsizian@gmail.com>
2017-01-20 09:32:46 -0500

    unfinished

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,42 @@
-angular
-  .module("whenPresident", [
-    "ui.router",
-    "ngResource"
-  ])
+(function(){
+  angular
+    .module("whenPresident", [
+      "ui.router",
+      "ngResource"
+    ])
+    .config([
+      "$stateProvider",
+      Router
+    ])
+    .factory("Candidate", [
+      "$resource",
+      Candidate
+    ])
+    .controller("indexCtrl", [
+      "Candidate",
+      indexController
+    ])
+
+  function Router($stateProvider){
+    $stateProvider
+      .state("welcome", {
+        url: "/",
+        templateUrl: "/assets/js/ng-views/welcome.html"
+      })
+      .state("index", {
+        url: "/candidates",
+        templateUrl: "/assets/js/ng-views/index.html",
+        controller: "indexCtrl",
+        controllerAs: "vm"
+      })
+  }
+  function Candidate ($resource) {
+      return $resource("/api/candidates/:name", {}, {
+        update: { method: "PUT" }
+      });
+    }
+
+  function indexController (Candidate) {
+    this.candidates = Candidate.query()
+  }
+})();
